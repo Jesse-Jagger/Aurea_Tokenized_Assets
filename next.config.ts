c0f114b64b176baecd9e3e8bc648dad7a8419f13
Jesse-Jagger <jessejackson.055@gmail.com>
2025-11-25 09:00:38 +0000

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "unenamelled-merrill-vicissitudinous.ngrok-free.dev",
-      },
-    ],
-  },
-  output: "standalone",
+  output: 'export',
 };
 
 export default nextConfig;
